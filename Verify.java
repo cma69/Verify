d7ea42dbe7cb14faece7ea6c90f196892b111571
Simon Legesse <sbl2466@email.vccs.edu>
2017-10-11 09:34:22 -0400

    init

---
+++ Verify.java
@@ -0,0 +1,81 @@
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+
+import java.util.Optional;
+
+import javafx.scene.control.TextInputDialog;
+
+
+
+public class Verify extends Application {
+
+
+
+    @Override
+
+    public void start(Stage primaryStage) {
+
+
+
+        String usr, pass;
+
+
+
+        TextInputDialog textInputDialog = new TextInputDialog("");
+
+
+
+        textInputDialog.setTitle("Authentication");
+
+
+
+        textInputDialog.setContentText("Enter your username ");
+
+        Optional<String> result = textInputDialog.showAndWait();
+
+        String usrinput = result.get();
+
+
+
+
+
+        textInputDialog.setContentText("Enter your password ");
+
+        Optional<String> result2 = textInputDialog.showAndWait();
+
+        String passinput = result2.get();
+
+
+
+
+
+        if (usrinput.equals("simon") && (passinput.equals("697865"))) {
+
+            System.out.print("Welcome simon");
+
+        } else {
+
+            System.out.print("Wrong password or username,please try again");
+
+
+
+        }
+
+
+
+
+
+    }
+
+    public static void main(String [] args) {
+
+        launch(args);
+
+
+
+
+
+    }
+
+}
